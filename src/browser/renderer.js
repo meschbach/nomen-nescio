aa22fa216ae5297ebc3b93e6bf73a4dd8eecb9fd
Mark Eschbach <meschbach@gmail.com>
2015-04-03 12:09:07 -0700

    Breaking out ThreeJS integration

---
+++ src/browser/renderer.js
@@ -0,0 +1,33 @@
+function init_gl(){
+	var scene = new THREE.Scene();
+	var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+	var renderer = new THREE.WebGLRenderer();
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
+
+	camera.position.z = 5;
+
+	var renderingStats = init_stats();
+	var animatables = [];
+	var render = function () {
+		requestAnimationFrame( render );
+
+		renderingStats.begin();
+
+		animatables.forEach( function( visual ){
+			visual.animate_tick( scene );
+		});
+
+		renderingStats.end();
+
+		renderer.render(scene, camera);
+	};
+
+	render();
+	return {
+		camera: camera,
+		animatable: animatables
+	};
+}
--- src/browser/rendering.js
+++ src/browser/rendering.js
@@ -417,37 +417,3 @@ var print = true;
 			var sim_clock = setInterval( game_tick, 1000 / 60 );
 			return { simulate: tickables, collidables: collidables };
 		}
-
-			function init_gl(){
-				var scene = new THREE.Scene();
-				var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-
-				var renderer = new THREE.WebGLRenderer();
-				renderer.setPixelRatio( window.devicePixelRatio );
-				renderer.setSize( window.innerWidth, window.innerHeight );
-				document.body.appendChild( renderer.domElement );
-
-				camera.position.z = 5;
-
-				var renderingStats = init_stats();
-				var animatables = [];
-				var render = function () {
-					requestAnimationFrame( render );
-
-					renderingStats.begin();
-
-					animatables.forEach( function( visual ){
-						visual.animate_tick( scene );
-					});
-
-					renderingStats.end();
-
-					renderer.render(scene, camera);
-				};
-
-				render();
-				return {
-					camera: camera,
-					animatable: animatables
-				};
-			}
