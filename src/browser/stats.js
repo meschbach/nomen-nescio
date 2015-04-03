ee3d04b0e224f7ee73c8a59d0245408c32516491
Mark Eschbach <meschbach@gmail.com>
2015-04-03 12:32:34 -0700

    Display FPS by default

--- src/browser/stats.js
+++ src/browser/stats.js
@@ -3,9 +3,6 @@ function init_stats( targetID ){
 	 * Wire in the stats library
 	 */
 	var stats = new Stats();
-	stats.setMode(1); // 0: fps, 1: ms
-
-	// align top-left
 	stats.domElement.style.display = 'inline-block';
 
 	document.getElementById( targetID || "stats-container" ).appendChild( stats.domElement );
