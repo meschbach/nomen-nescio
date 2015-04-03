d019324b2554f47cbce9dcce6d297f56f47ec4a2
Mark Eschbach <meschbach@gmail.com>
2015-04-03 12:21:12 -0700

    Reformating since move from inline of the file

--- src/browser/rendering.js
+++ src/browser/rendering.js
@@ -1,34 +1,34 @@
 //http://threejs.org/docs/index.html#Manual/Introduction/Creating_a_scene
-		function init(){
-			var sys = {
-				visuals: init_gl(),
-				sim: init_simulation( )
-			};
+function init(){
+	var sys = {
+		visuals: init_gl(),
+		sim: init_simulation( )
+	};
 //			init_skybox( sys );
-			var player = init_player( sys );
-			init_player_controls( sys, player.actions );
+	var player = init_player( sys );
+	init_player_controls( sys, player.actions );
 
-			var dock = init_dock( sys );
+	var dock = init_dock( sys );
 
-			//Level
-			var done = false;
-			player.collidedWith = function( other ){
-				if( !done ){
-					if( other.object == dock ){
-						done = true;
-						console.log( "Docking complete. Hmm...we need more objectives." );
-					}
-				}
-			}
-
-			var textGeom;
-			var text = {
-				animate_tick: function( scene ){
-					if( !textGeom ){
-						textGeom = new THREE.TextGeometry( "Hello, intersect with the cube." );
-						scene.add( textGeom );
-					}
-					textGeom.position.y = 1.2;
-				}
+	//Level
+	var done = false;
+	player.collidedWith = function( other ){
+		if( !done ){
+			if( other.object == dock ){
+				done = true;
+				console.log( "Docking complete. Hmm...we need more objectives." );
 			}
 		}
+	}
+
+	var textGeom;
+	var text = {
+		animate_tick: function( scene ){
+			if( !textGeom ){
+				textGeom = new THREE.TextGeometry( "Hello, intersect with the cube." );
+				scene.add( textGeom );
+			}
+			textGeom.position.y = 1.2;
+		}
+	}
+}
