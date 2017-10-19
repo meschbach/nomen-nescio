6eeb7243f7389f3ae4873129e3ab69c6f7066ca6
Mark Eschbach <meschbach@gmail.com>
2017-10-18 22:33:56 -0700

    Production webpack configuration now installed

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,31 @@
- var path = require('path');
- var webpack = require('webpack');
+var path = require('path');
+var webpack = require('webpack');
 
- module.exports = {
-     entry:  './src/browser/rendering.js',
-     output: {
-         path: path.resolve(__dirname, 'htdocs'),
-         filename: 'application.js',
-				 library: 'nn'
-     },
-     module: {
-         loaders: [
-             {
-                 test: /\.js$/,
-                 loader: 'babel-loader',
-                 query: {
-                 }
-             }
-         ]
-     },
-     stats: {
-         colors: true
-     },
-     devtool: 'source-map'
- };
+let debug =  process.env['NODE_ENV'] != "production"
+
+module.exports = {
+	entry:  './src/browser/rendering.js',
+	output: {
+		path: path.resolve(__dirname, 'htdocs'),
+		filename: 'application.js',
+		library: 'nn'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				query: { }
+			}
+		]
+	},
+	stats: {
+		colors: true
+	},
+	devtool: 'source-map',
+	plugins: debug ? [] :  [
+		new webpack.optimize.OccurrenceOrderPlugin(),
+		new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+	],
+};
 
