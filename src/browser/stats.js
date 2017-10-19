23d962d42f72ee2eec2b2f12e8df8c604d3e782a
Mark Eschbach <meschbach@gmail.com>
2017-10-18 20:36:53 -0700

    Fixing styling of the stats bars

--- src/browser/stats.js
+++ src/browser/stats.js
@@ -5,7 +5,8 @@ export function init_stats( targetID ){
 	 * Wire in the stats library
 	 */
 	var stats = new Stats();
-	stats.domElement.style.display = 'inline-block';
+	stats.domElement.style.display = 'inline-block'
+	stats.domElement.style.position = 'inherit'
 
 	document.getElementById( targetID || "stats-container" ).appendChild( stats.domElement );
 	return stats;
