fdc3d8533a3e8b03a6c4a5b01aa00e3dd5785702
Mark Eschbach <meschbach@gmail.com>
2017-10-18 20:11:01 -0700

    Removes app and lib concatentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,6 @@ module.exports = function( grunt ){
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		concat: {
-			app : {
-				src: ["src/browser/*.js"],
-				dest: "htdocs/application.js"
-			},
 			designer: {
 				src: ["src/ship-designer/**/*.js"],
 				dest: "htdocs/designer/ship-designer.js"
@@ -14,20 +10,8 @@ module.exports = function( grunt ){
 				src: ["src/browser/*.css"],
 				dest: "htdocs/application.css"
 			},
-			libs: {
-				src: [
-					"bower_components/threejs/build/three.js",
-					"bower_components/threejs/examples/js/loaders/ColladaLoader.js",
-					"bower_components/stats.js/build/stats.min.js"
-				],
-				dest: "htdocs/libs.js"
-			}
 		},
 		watch : {
-			app_js: {
-				files: ["src/browser/*.js"],
-				tasks: ["concat:app"]
-			},
 			app_css: {
 				files: ["src/browser/*.css"],
 				tasks: "concat:app_css"
