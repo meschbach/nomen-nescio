15980135dcc4ee21595b77334db31640732749f2
Mark Eschbach <meschbach@gmail.com>
2017-10-22 19:06:07 -0700

    Pulled out System class from initializer

--- src/browser/system.js
+++ src/browser/system.js
@@ -1,18 +1,22 @@
 import { init_gl } from './renderer'
 import { init_simulation } from './simulation'
 
-export function init_system(){
-	function System(){ }
-	System.prototype.init = function(){
+class System {
+	constructor() {}
+
+	init() {
 		this.visuals = init_gl();
 		this.sim = init_simulation();
 	}
-	System.prototype.attach = function( what ){
+
+	attach( what ){
 		if( what.simulation_tick ){ this.sim.tickables.push( what ); }
 		if( what.collision_tick ){ this.sim.collidables.push( what ); }
 		if( what.animate_tick ){ this.visuals.animatable.push( what ); }
 	}
+}
 
+export function init_system(){
 	var sys = new System();
 	return sys;
 }
